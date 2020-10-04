69b5052fc1d499a36ed12a830f4a4932bfbeef0b
DevFantasyYT <60735010+DevFantasyYT@users.noreply.github.com>
2020-10-04 17:51:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const botConfig = require("./botconfig.json");
 const fs = require('fs');
 
 const client = new discord.Client();
-client.login(process.inv.token);
+client.login(process.env.token);
 
 client.commands = new discord.Collection();
 
