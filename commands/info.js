bbd365fdeadbf8b57d24f0ed8dc15205718f1a5a
DevFantasyYT <60735010+DevFantasyYT@users.noreply.github.com>
2020-10-09 17:02:46 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -2,16 +2,17 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-    var roles = message.Guild.roles;
+    var serverIcon = message.guild.iconURL();
 
     var serverInfo = new discord.MessageEmbed()
     .setTitle("Server Informatie")
     .addFields(
         {name: "Server naam:", value:`${message.guild.name}`},
         {name: "Aantal mensen op de server:", value:`${message.guild.memberCount}`},
-        {name: "Rollen:", value:`${roles}`},
+        {name: "Rollen:", value:`${message.guild.roles.cache.size}`},
     )
     .setColor("#be2ee6")
+    .setThumbnail(serverIcon)
     .setTimestamp();
 
 return message.channel.send(serverInfo);
